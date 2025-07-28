ac171689bce1c5db6bc688a5f60867f5c6a343f4
Eduardo <tkd13000@gmail.com>
2025-07-28 12:47:27 -0600

    Git hub pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Gato/', // nombre del repo
+  base: '/Gato/',
+  plugins: [react()],
 })
-
